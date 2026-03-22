1f6b30fff7cc4d78a6d3cf0caefd71501fdd9ec1
Delphix20 <paulcrapatureanu@gmail.com>
2026-03-22 20:10:11 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -66,18 +66,6 @@
 			});
 
 	// Header.
-		if ($banner.length > 0
-		&&	$header.hasClass('alt')) {
-
-			$window.on('resize', function() { $window.trigger('scroll'); });
-
-			$banner.scrollex({
-				bottom:		$header.outerHeight() + 1,
-				terminate:	function() { $header.removeClass('alt'); },
-				enter:		function() { $header.addClass('alt'); },
-				leave:		function() { $header.removeClass('alt'); }
-			});
-
-		}
+		$header.addClass('alt');
 
 })(jQuery);
